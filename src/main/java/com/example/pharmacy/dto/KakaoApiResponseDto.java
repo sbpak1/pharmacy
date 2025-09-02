35a53150b908129ddbea0728d46cb5d44656a0e7
sbpak1 <sbpak1@gmail.com>
2025-09-02 12:06:00 +0900

    KAKAO_API에서 받을 DTO 구성

---
+++ src/main/java/com/example/pharmacy/dto/KakaoApiResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.pharmacy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class KakaoApiResponseDto {
+    @JsonProperty("documents")
+    private List<DocumentDto> documentList;
+    @JsonProperty("meta")
+    private MetaDto metaDto;
+}
